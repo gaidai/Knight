97fa6394af5ef85143c6d99fb3b17031cae3d342
gaidai <q-daddy@mail.ru>
2017-04-13 20:06:01 +0300

    using ClassPathXmlApplicationContext

--- src/main/java/com/sgaidai/knight/BraveKnight.java
+++ src/main/java/com/sgaidai/knight/BraveKnight.java
@@ -1,27 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.sgaidai.knight;
 
-/**
- *
- * @author user
- */
+
 public class BraveKnight implements Knight {
     public Quest quest;
-    public Minstrel minstrel;
+    
     
     public BraveKnight(Quest quest, Minstrel minstrel) {
         this.quest = quest;
-        this.minstrel = minstrel;
-    }
+        }
+    
     @Override
     public void embarkOnQuest()  {
-        
         quest.embark(); 
         
     }
-    
 }
--- src/main/java/com/sgaidai/knight/Main.java
+++ src/main/java/com/sgaidai/knight/Main.java
@@ -1,23 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.sgaidai.knight;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-
+import java.io.FileNotFoundException;
 
 
 public class Main {
     
     public static void main(String[] args) {
-        // Загрузка контекста Spring
-        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/knights.xml");
-        // Получение компонента knight
+        ApplicationContext context = new ClassPathXmlApplicationContext("knights.xml");
+       // FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/sgaidai/knights.xml");
         Knight knight = (Knight) context.getBean("knight");
-        // Использование компонента knight
+        
         knight.embarkOnQuest();
     }
 
